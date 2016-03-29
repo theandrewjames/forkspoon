4b6592512ae0e511a7b6405f2b79cfb0f76ad867
Andrew <theandrewjames@aol.com>
2016-03-29 11:25:21 -0700

    Search now displays restaurants. #1

--- app.js
+++ app.js
@@ -8,36 +8,46 @@ var restaurants = [
     name: "Yojimbo",
     website: "yojimbosushi.com",
     phoneNumber: "(510) 523-4120",
-    address: "1221 Park St, Alameda, CA 94501",
-    type: "sushi"
+    address: "1221 Park St, Alameda, CA, 94501",
+    type: "Sushi",
+    images: ["images/yojimbo1.jpg", "images/yojimbo2.jpg"],
+    dataId: 0
   },
   {
     name: "SPIN! Neopolitan Pizza",
     website: "spinpizza.com",
     phoneNumber: "(510) 769-7492",
-    address: "2670 5th St, Alameda, CA 94501",
-    type: "italian"
+    address: "2670 5th St, Alameda, CA, 94501",
+    type: "Italian",
+    images: ["images/spin1.jpg", "images/spin2.jpg"],
+    dataId: 1
   },
   {
     name: "Lola's Chicken Shack",
     website: "lolaschickenshack.com",
     phoneNumber: "(510) 521-4488",
-    address: "1417 Park St, Alameda, CA 94501",
-    type: "chicken"
+    address: "1417 Park St, Alameda, CA, 94501",
+    type: "Chicken",
+    images: ["images/lolas1.jpg", "images/lolas2.jpg"],
+    dataId: 2
   },
   {
     name: "Trabocco Kitchen and Cocktails",
     website: "trabocco.com",
     phoneNumber: "(510) 521-1152",
-    address: "2213 S Shore Ctr, Alameda, CA 94501",
-    type: "italian"
+    address: "2213 S Shore Ctr, Alameda, CA, 94501",
+    type: "Italian",
+    images: ["images/trabocco1.jpg", "images/trabocco2.jpg"],
+    dataId: 3
   },
   {
     name: "Mama Papa Lithuania Restaurant",
     website: "mamapapalithuania.com",
     phoneNumber: "(510) 522-4100",
-    address: "1241 Park St, Alameda, CA 94501",
-    type: "european"
+    address: "1241 Park St, Alameda, CA, 94501",
+    type: "European",
+    images: ["images/mamapapa1.jpg", "images/mamapapa2.jpg"],
+    dataId: 4
   }
 ];
 
@@ -46,9 +56,11 @@ var matchedRestaurants = [];
 app.use(express.static("./"));
 
 app.get("/search", function(req, res) {
-  console.log(req.query);
+  while(matchedRestaurants.length > 0) {
+    matchedRestaurants.pop();
+  }
   for(var i = 0;i < restaurants.length;i++) {
-    if(req.query.q == restaurants[i].type || req.query.q == restaurants[i].name.toLowerCase()) {
+    if(req.query.q == restaurants[i].type.toLowerCase() || req.query.q == restaurants[i].name.toLowerCase()) {
       matchedRestaurants.push(restaurants[i])
     }
   } if(matchedRestaurants.length > 0) {
--- default.js
+++ default.js
@@ -1,4 +1,5 @@
 var search = document.getElementById("search-box");
+var resultsDiv = document.getElementById("results");
 
 search.addEventListener("click", function() {
   var searchValue = search.value.toLowerCase();
@@ -7,9 +8,33 @@ search.addEventListener("click", function() {
   xhr.send();
   xhr.onload = function() {
     if(xhr.status == 200) {
+
       var results = JSON.parse(xhr.responseText);
+      console.log(results)
       for(var i = 0;i < results.length;i++) {
-        console.log(results[i]);
+        var resultRow = document.createElement("div");
+        var panel = document.createElement("div");
+        var image = document.createElement("img");
+        var nameDiv = document.createElement("div");
+        var row = document.createElement("row");
+        var columnDiv = document.createElement("div");
+        var addressDiv = document.createElement("div");
+        nameDiv.innerHTML = results[i].name + "<br>" + results[i].type;
+        addressDiv.innerHTML = results[i].address + "<br>" + results[i].phoneNumber;
+        image.setAttribute("src", results[i].images[0]);
+        nameDiv.className = "nameDiv col-md-5";
+        image.className = "img-responsive resultImage";
+        resultRow.className = "row";
+        panel.className = "col-md-9 panel panel-default result-panel";
+        columnDiv.className = "col-md-12 panel-body";
+        addressDiv.className = "col-md-3 addressDiv";
+        columnDiv.appendChild(image);
+        columnDiv.appendChild(nameDiv);
+        columnDiv.appendChild(addressDiv);
+        row.appendChild(columnDiv);
+        panel.appendChild(row);
+        resultRow.appendChild(panel)
+        resultsDiv.appendChild(resultRow);
       }
     }
   }
