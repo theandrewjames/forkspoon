4489a0e2853c3861f8b143c7f40c603c2400ab9b
Andrew <theandrewjames@aol.com>
2016-03-31 11:27:08 -0700

    Add review panel appears under each restaurant. #3

--- default.js
+++ default.js
@@ -23,17 +23,44 @@ search.addEventListener("click", function() {
         var row = document.createElement("row");
         var columnDiv = document.createElement("div");
         var addressDiv = document.createElement("div");
+        var writeReviewPanel = document.createElement("div");
+        var writeReviewForm = document.createElement("form");
+        var nameFormGroup = document.createElement("div");
+        var reviewerNameInput = document.createElement("input");
+        var reviewFormGroup = document.createElement("div");
+        var reviewTextArea = document.createElement("textarea");
+        var submitReviewButton = document.createElement("button");
         nameDiv.innerHTML = "<b>" + "<a class=restaurantTitle data-id=" + results[i].dataId + ">"+ results[i].name + "</a>" + "</b>" + "<br>" + "<a href="+
         "http://" + results[i].website+">" + results[i].website + "</a>" + "<br>" + results[i].cost;
         addressDiv.innerHTML = results[i].address + "<br>" + results[i].phoneNumber;
         image.setAttribute("src", results[i].images[0]);
+        resultRow.setAttribute("data-id", results[i].dataId);
+        reviewerNameInput.setAttribute("type", "text");
+        reviewerNameInput.setAttribute("placeholder", "Name");
+        reviewerNameInput.setAttribute("data-id", results[i].dataId)
+        reviewTextArea.setAttribute("rows", 2);
+        reviewTextArea.setAttribute("placeholder", "Type review here.");
+        reviewTextArea.setAttribute("data-id", results[i].dataId)
+        submitReviewButton.setAttribute("type", "submit");
+        submitReviewButton.textContent = "Submit Review"
         nameDiv.className = "nameDiv col-md-6";
         image.className = "img-responsive resultImage img-rounded";
         resultRow.className = "row itemRow";
-        resultRow.setAttribute("data-id", results[i].dataId);
         panel.className = "col-md-9 panel panel-default result-panel";
         columnDiv.className = "col-md-12 panel-body";
         addressDiv.className = "col-md-3 addressDiv";
+        writeReviewPanel.className = "col-md-8 panel panel-default panel-body";
+        nameFormGroup.className = "form-group";
+        reviewerNameInput.className = "form-control reviewerName";
+        reviewFormGroup.className = "form-group";
+        reviewTextArea.className = "form-control reviewerReview";
+        submitReviewButton.className = "btn btn-default submitReview";
+        nameFormGroup.appendChild(reviewerNameInput);
+        writeReviewForm.appendChild(nameFormGroup);
+        reviewFormGroup.appendChild(reviewTextArea);
+        writeReviewForm.appendChild(reviewFormGroup);
+        writeReviewForm.appendChild(submitReviewButton)
+        writeReviewPanel.appendChild(writeReviewForm)
         columnDiv.appendChild(image);
         columnDiv.appendChild(nameDiv);
         columnDiv.appendChild(addressDiv);
@@ -41,6 +68,7 @@ search.addEventListener("click", function() {
         panel.appendChild(row);
         resultRow.appendChild(panel);
         resultsDiv.appendChild(resultRow);
+        resultRow.appendChild(writeReviewPanel)
         for(var x = 0;x < results[i].reviews.length;x++) {
           var reviewsDiv = document.createElement("div");
           var reviewerName = document.createElement("div");
